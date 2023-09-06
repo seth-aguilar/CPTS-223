4cee08fd4b1cdedc6671ff2e658b2e45e841008d
seth-aguilar <sethcaguilar@gmail.com>
2023-09-05 18:26:50 -0700

    Setup

---
+++ PA1/main.cpp
@@ -0,0 +1,13 @@
+// code source: https://www.codesdope.com/blog/article/c-linked-lists-in-c-singly-linked-list/
+
+#include <iostream>
+#include "linked_list.h"
+#include "node.h"
+
+int main()
+{
+    linked_list a;
+    a.add_node(1);
+    a.add_node(2);
+    return 0;
+}
