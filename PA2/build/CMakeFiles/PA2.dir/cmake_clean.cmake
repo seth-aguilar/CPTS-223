0916c2b1333e40f8e0b9d676a185556c56873b6b
seth-aguilar <sethcaguilar@gmail.com>
2023-10-10 16:37:36 -0700

    Cmake

---
+++ PA2/build/CMakeFiles/PA2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA2.dir/AVLTree.cpp.o"
+  "CMakeFiles/PA2.dir/AVLTree.cpp.o.d"
+  "CMakeFiles/PA2.dir/main.cpp.o"
+  "CMakeFiles/PA2.dir/main.cpp.o.d"
+  "PA2"
+  "PA2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
